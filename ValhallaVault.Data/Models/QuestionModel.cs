7c7ca9e782b837005c42ee22cecf0436069e87b3
MaxTarn <Maxtharn@gmail.com>
2024-03-04 12:48:08 +0100

    added QuestionModel , added AnswerModel, Added Questions which contains some dummy data of QuestionModel

---
+++ ValhallaVault.Data/Models/QuestionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValhallaVault.Data.Models;
+
+public class QuestionModel
+{
+    public string Question { get; set; } = null!;
+    public List<AnswerModel> Answers { get; set; } = null!;
+
+
+}
+
