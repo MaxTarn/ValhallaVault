b90adbf52c4ce02068315adf8b37a77a9d55e715
miiamole <miia_pelaez@hotmail.com>
2024-03-04 13:47:26 +0100

    Bajskorvc

--- ValhallaVault/Program.cs
+++ ValhallaVault/Program.cs
@@ -35,11 +35,67 @@ namespace ValhallaVault
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()  //HÄR LÄGGER VI TILL DENNA RADEN 
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddSignInManager()
                 .AddDefaultTokenProviders();
 
             builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+
+
+
+
+            //skapa users och roller som ska finnas med från start
+
+            using (ServiceProvider sp = builder.Services.BuildServiceProvider())
+            {
+                var context = sp.GetRequiredService<ApplicationDbContext>(); //plocka ut dessa ut vår dependency injection container
+                var signInManager = sp.GetRequiredService<SignInManager<ApplicationUser>>();
+                var roleManagaer = sp.GetRequiredService<RoleManager<IdentityRole>>();
+
+                context.Database.Migrate(); //har vi inte skapat databasen så görs det nu
+
+                ApplicationUser newUser = new()
+                {
+                    UserName = "admin",
+                    Email = "adminuser@mail.com",
+                    EmailConfirmed = true
+                };
+                var user = signInManager.UserManager.FindByEmailAsync(newUser.Email).GetAwaiter().GetResult(); //vi vill att det ska köras synkront, därvav de två sista metoderna
+                if (user == null)
+                {
+                    //skapa en ny user
+                    signInManager.UserManager.CreateAsync(newUser, "Password1234!").GetAwaiter().GetResult();
+
+
+
+                    //kolla om adminrollen existerar
+                    bool adminRoleExists = roleManagaer.RoleExistsAsync("Admin").GetAwaiter().GetResult();
+                    if (!adminRoleExists)
+                    {
+                        //edminrollen existerar ej, skapa den!
+
+                        IdentityRole adminRole = new()
+                        {
+                            Name = "Admin",
+                        };
+                        roleManagaer.CreateAsync(adminRole).GetAwaiter().GetResult();
+                    }
+                    //tilldela adminrollen till den nya användaren
+                    signInManager.UserManager.AddToRoleAsync(newUser, "Admin").GetAwaiter().GetResult();
+                }
+            }
+
+
+
+
+
+
+
+
+
+
 
             var app = builder.Build();
 
