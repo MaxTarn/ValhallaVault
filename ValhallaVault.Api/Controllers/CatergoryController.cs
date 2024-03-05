e57cc05de6710129772f13ecd91ba72827d3f6cb
LudvigBergerud <144774467+LudvigBergerud@users.noreply.github.com>
2024-03-05 16:37:38 +0100

    Started admin. Commented out controlers untill complete. Remove extra Category Controller

--- ValhallaVault.Api/Controllers/CatergoryController.cs
+++ ValhallaVault.Api/Controllers/CatergoryController.cs
@@ -4,94 +4,94 @@ using ValhallaVault.Data.Models;
 
 namespace ValhallaVault.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CatergoryController : ControllerBase
-    {
-        private readonly CatergoryRepository _catergoryRepo;
+    //[Route("api/[controller]")]
+    //[ApiController]
+    //public class CatergoryController : ControllerBase
+    //{
+    //    private readonly CatergoryRepository _catergoryRepo;
 
-        public CatergoryController(CatergoryRepository<CategoryModel> catergoryRepo)
-        {
-            _catergoryRepo = catergoryRepo;
-        }
+    //    public CatergoryController(CatergoryRepository<CategoryModel> catergoryRepo)
+    //    {
+    //        _catergoryRepo = catergoryRepo;
+    //    }
 
-        [HttpGet]
-        public async Task<IEnumerable<CategoryModel>> GetAll()
-        {
-            var categories = await _catergoryRepo.GetAll();
+    //    [HttpGet]
+    //    public async Task<IEnumerable<CategoryModel>> GetAll()
+    //    {
+    //        var categories = await _catergoryRepo.GetAll();
 
-            if (categories != null)
-            {
-                return Ok(categories);
-            }
+    //        if (categories != null)
+    //        {
+    //            return Ok(categories);
+    //        }
 
-            return (IEnumerable<CategoryModel>)NotFound("The categories could not be found");
-        }
-
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            var category = await _catergoryRepo.Find(a => a.Id == id);
-
-            if (category != null)
-            {
-                return Ok(category);
-            }
-
-            return NotFound("The category that you were looking for could not be found");
-        }
+    //        return (IEnumerable<CategoryModel>)NotFound("The categories could not be found");
+    //    }
 
 
-        [HttpPost]
-        public async Task<IActionResult> Post(CategoryModel category)
-        {
+    //    [HttpGet("{id}")]
+    //    public async Task<IActionResult> GetById(int id)
+    //    {
+    //        var category = await _catergoryRepo.Find(a => a.Id == id);
 
-            if (category != null)
-            {
-                await _catergoryRepo.Add(category);
+    //        if (category != null)
+    //        {
+    //            return Ok(category);
+    //        }
 
-                await _catergoryRepo.Complete();
-
-                return Ok(category);
-            }
-
-            return NotFound("The category that you were trying to post could not be found");
-        }
+    //        return NotFound("The category that you were looking for could not be found");
+    //    }
 
 
-        [HttpPut("{Id}")]
-        public async Task<IActionResult> Put(CategoryModel previousCategory, int Id)
-        {
-            var result = await _catergoryRepo.Get(previousCategory.Id);
+    //    [HttpPost]
+    //    public async Task<IActionResult> Post(CategoryModel category)
+    //    {
 
-            if (result != null)
-            {
-                result.Name = previousCategory.Name;
+    //        if (category != null)
+    //        {
+    //            await _catergoryRepo.Add(category);
 
+    //            await _catergoryRepo.Complete();
 
-                await _catergoryRepo.Complete();
+    //            return Ok(category);
+    //        }
 
-                return Ok(result);
-            }
-
-
-            return NotFound("The category that you wanted to update could not be found");
-        }
+    //        return NotFound("The category that you were trying to post could not be found");
+    //    }
 
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var category = await _catergoryRepo.Delete(id);
+    //    [HttpPut("{Id}")]
+    //    public async Task<IActionResult> Put(CategoryModel previousCategory, int Id)
+    //    {
+    //        var result = await _catergoryRepo.Get(previousCategory.Id);
 
-            if (category != null)
-            {
-                return Ok(category);
-            }
+    //        if (result != null)
+    //        {
+    //            result.Name = previousCategory.Name;
 
-            return NotFound("The category that you wanted to delete could not be found");
-        }
 
-    }
+    //            await _catergoryRepo.Complete();
+
+    //            return Ok(result);
+    //        }
+
+
+    //        return NotFound("The category that you wanted to update could not be found");
+    //    }
+
+
+    //    [HttpDelete("{id}")]
+    //    public async Task<IActionResult> Delete(int id)
+    //    {
+    //        var category = await _catergoryRepo.Delete(id);
+
+    //        if (category != null)
+    //        {
+    //            return Ok(category);
+    //        }
+
+    //        return NotFound("The category that you wanted to delete could not be found");
+    //    }
+
+    //}
 }
--- ValhallaVault.Api/Controllers/SubcategoryController.cs
+++ ValhallaVault.Api/Controllers/SubcategoryController.cs
@@ -4,92 +4,92 @@ using ValhallaVault.Data.Models;
 
 namespace ValhallaVault.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SubcategoryController : ControllerBase
-    {
-        private readonly SubcategoryRepository _subcategoryRepo;
+    //[Route("api/[controller]")]
+    //[ApiController]
+    //public class SubcategoryController : ControllerBase
+    //{
+    //    private readonly SubcategoryRepository _subcategoryRepo;
 
-        public SubcategoryController(SubcategoryRepository<SubcategoryModel> subcategoryRepo)
-        {
-            _subcategoryRepo = subcategoryRepo;
-        }
+    //    public SubcategoryController(SubcategoryRepository<SubcategoryModel> subcategoryRepo)
+    //    {
+    //        _subcategoryRepo = subcategoryRepo;
+    //    }
 
-        [HttpGet]
-        public async Task<IEnumerable<SubcategoryModel>> GetAll()
-        {
-            var subcategorys = await _subcategoryRepo.GetAll();
+    //    [HttpGet]
+    //    public async Task<IEnumerable<SubcategoryModel>> GetAll()
+    //    {
+    //        var subcategorys = await _subcategoryRepo.GetAll();
 
-            if (subcategorys != null)
-            {
-                return Ok(subcategorys);
-            }
+    //        if (subcategorys != null)
+    //        {
+    //            return Ok(subcategorys);
+    //        }
 
-            return (IEnumerable<SubcategoryModel>)NotFound("The subcategories could not be found");
-        }
-
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            var subcategory = await _subcategoryRepo.Find(a => a.Id == id);
-
-            if (subcategory != null)
-            {
-                return Ok(subcategory);
-            }
-
-            return NotFound("The subcategory that you were looking for could not be found");
-        }
+    //        return (IEnumerable<SubcategoryModel>)NotFound("The subcategories could not be found");
+    //    }
 
 
-        [HttpPost]
-        public async Task<IActionResult> Post(SubcategoryModel subcategory)
-        {
+    //    [HttpGet("{id}")]
+    //    public async Task<IActionResult> GetById(int id)
+    //    {
+    //        var subcategory = await _subcategoryRepo.Find(a => a.Id == id);
 
-            if (subcategory != null)
-            {
-                await _subcategoryRepo.Add(subcategory);
+    //        if (subcategory != null)
+    //        {
+    //            return Ok(subcategory);
+    //        }
 
-                await _subcategoryRepo.Complete();
-
-                return Ok(subcategory);
-            }
-
-            return NotFound("The subcategory that you were trying to post could not be found");
-        }
+    //        return NotFound("The subcategory that you were looking for could not be found");
+    //    }
 
 
-        [HttpPut("{Id}")]
-        public async Task<IActionResult> Put(SubcategoryModel previousSubcategory, int Id)
-        {
-            var result = await _subcategoryRepo.Get(previousSubcategory.Id);
+    //    [HttpPost]
+    //    public async Task<IActionResult> Post(SubcategoryModel subcategory)
+    //    {
 
-            if (result != null)
-            {
-                result.Name = previousSubcategory.Name;
+    //        if (subcategory != null)
+    //        {
+    //            await _subcategoryRepo.Add(subcategory);
 
-                await _subcategoryRepo.Complete();
+    //            await _subcategoryRepo.Complete();
 
-                return Ok(result);
-            }
+    //            return Ok(subcategory);
+    //        }
 
-            return NotFound("The subcategory that you wanted to update could not be found");
-        }
+    //        return NotFound("The subcategory that you were trying to post could not be found");
+    //    }
 
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var subcategory = await _subcategoryRepo.Delete(id);
+    //    [HttpPut("{Id}")]
+    //    public async Task<IActionResult> Put(SubcategoryModel previousSubcategory, int Id)
+    //    {
+    //        var result = await _subcategoryRepo.Get(previousSubcategory.Id);
 
-            if (subcategory != null)
-            {
-                return Ok(subcategory);
-            }
+    //        if (result != null)
+    //        {
+    //            result.Name = previousSubcategory.Name;
 
-            return NotFound("The subcategory that you wanted to delete could not be found");
-        }
+    //            await _subcategoryRepo.Complete();
 
-    }
+    //            return Ok(result);
+    //        }
+
+    //        return NotFound("The subcategory that you wanted to update could not be found");
+    //    }
+
+
+    //    [HttpDelete("{id}")]
+    //    public async Task<IActionResult> Delete(int id)
+    //    {
+    //        var subcategory = await _subcategoryRepo.Delete(id);
+
+    //        if (subcategory != null)
+    //        {
+    //            return Ok(subcategory);
+    //        }
+
+    //        return NotFound("The subcategory that you wanted to delete could not be found");
+    //    }
+
+    //}
 }
