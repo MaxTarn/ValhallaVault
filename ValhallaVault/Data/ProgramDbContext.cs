5ee25b8b69c7e4e1dd86744799775c6c65cbcd38
Henk01 <146067073+Henk01@users.noreply.github.com>
2024-03-05 10:28:46 +0100

    New DbContext

---
+++ ValhallaVault/Data/ProgramDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValhallaVault.Data.Models;
+
+namespace ValhallaVault.Data
+{
+    public class ProgramDbContext : DbContext
+    {
+        public ProgramDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<AnswerModel> Answers { get; set; }
+        DbSet<QuestionModel> Questions { get; set; }
+        DbSet<CategoryModel> Categories { get; set; }
+        DbSet<SubcategoryModel> Subcategories { get; set; }
+        DbSet<SegmentModel> Segments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryModel>()
+                .HasMany(c => c.Segments)
+                .WithOne(s => s.Category)
+                .HasForeignKey(s => s.CategoryModelId);
+
+            modelBuilder.Entity<SegmentModel>()
+                .HasMany(s => s.Subcategories)
+                .WithOne(s => s.Segment)
+                .HasForeignKey(s => s.SegmentModelId);
+
+            modelBuilder.Entity<SubcategoryModel>()
+                .HasMany(s => s.Questions)
+                .WithOne(q => q.Subcategory)
+                .HasForeignKey(q => q.SubcategoryModelId);
+
+            modelBuilder.Entity<QuestionModel>()
+                .HasMany(q => q.Answers)
+                .WithOne(s => s.Question)
+                .HasForeignKey(q => q.QuestionModelId);
+        }
+    }
