4dad07284db254b12bb70dcc7507d3a9ac1f346d
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-03-07 23:07:50 +0100

    added a new property in the AnswerModel, which will not store anything the db, but will be used for the simplicity of use in the UI

--- ValhallaVault.Data/Models/AnswerModel.cs
+++ ValhallaVault.Data/Models/AnswerModel.cs
@@ -13,5 +13,20 @@ public class AnswerModel
     public QuestionModel? Question { get; set; }
     public bool? IsCorrect { get; set; }
 
+
+
+
+
+    /// <summary>
+    /// This will not store any data in the DataBase.
+    /// Only used when getting data and knowing if the user has ticked in this particular answer.
+    /// [[ Yanky as all hell ]]
+    /// but most likely the easiest way to do this(if it actually works the way im thinking?? hopefully)
+    /// Ask Max if this should be deleted before deleting this 
+    /// </summary>
+    public bool? IsChecked { get; set; }
+
+
+
 }
 
