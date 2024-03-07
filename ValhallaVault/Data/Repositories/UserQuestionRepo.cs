35445e0275ba3cbfda012f1f7e223dec5fdf32eb
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-03-08 00:31:10 +0100

    added Repo  for UserQuestion since someone forgot to make one for this table?

---
+++ ValhallaVault/Data/Repositories/UserQuestionRepo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValhallaVault.Data.Models;
+
+namespace ValhallaVault.Data.Repositories;
+
+
+public class UserQuestionRepo
+{
+    //TODO add Crud operations
+    private readonly ProgramDbContext _dbContext;
+
+    public UserQuestionRepo(ProgramDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public List<UserQuestionModel> GetAll()
+    {
+        return _dbContext.UserQuestions.ToList();
+    }
+
+    public void Add(UserQuestionModel addThis)
+    {
+        _dbContext.UserQuestions.Add(addThis);
+    }
+
+    public async Task Save()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
