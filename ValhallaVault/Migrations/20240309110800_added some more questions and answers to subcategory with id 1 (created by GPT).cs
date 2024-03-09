3ee687e17bd9db62165da9b3d77cc10e0f1badf3
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-03-09 12:19:07 +0100

    deleted old migrations, remade database since it was not happy with the new sample data. database is fully operational

--- ValhallaVault/Migrations/20240309110800_added some more questions and answers to subcategory with id 1 (created by GPT).cs
+++ ValhallaVault/Migrations/20240309110800_added some more questions and answers to subcategory with id 1 (created by GPT).cs
@@ -7,7 +7,7 @@
 namespace ValhallaVault.Migrations
 {
     /// <inheritdoc />
-    public partial class addedAnotherPropertyInTheAnswerModel : Migration
+    public partial class addedsomemorequestionsandanswerstosubcategorywithid1createdbyGPT : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -199,7 +199,17 @@ namespace ValhallaVault.Migrations
                     { 6, "Maersk utsattes för NotPetya ransomware-angreppet, som ledde till omfattande störningar och förluster genom att kryptera företagets globala system.", "Vilken typ av malware var primärt ansvarig för denna incident?", 16 },
                     { 7, "DDoS-attacker kräver ofta betydande resurser och koordinering, vilket är karakteristiskt för organiserade cyberbrottsliga grupper.", "Vilken typ av aktör är mest sannolikt ansvarig för denna typ av attack?", 17 },
                     { 8, "Utbildning i digital säkerhet är avgörande för att hjälpa anställda att känna igen och undvika säkerhetshot som phishing, vilket är en vanlig attackvektor.", "Vilken åtgärd bör prioriteras för att förhindra framtida incidenter?", 18 },
-                    { 9, "Säkerhetsskyddslagen är en svensk lagstiftning som syftar till att skydda nationellt känslig information från spioneri, sabotage, terroristbrott och andra säkerhetshot.", "Vilken lagstiftning ger ramverket för detta skydd?", 19 }
+                    { 9, "Säkerhetsskyddslagen är en svensk lagstiftning som syftar till att skydda nationellt känslig information från spioneri, sabotage, terroristbrott och andra säkerhetshot.", "Vilken lagstiftning ger ramverket för detta skydd?", 19 },
+                    { 11, "Omedelbart kontakta ditt kreditkortsföretag för att rapportera de obehöriga transaktionerna och blockera ditt kort för att förhindra ytterligare skador.", "Vad ska du göra om du upptäcker obehöriga transaktioner på ditt kreditkort?", 1 },
+                    { 12, "Phishing-webbplatser och skadlig programvara är vanliga metoder för att stjäla kreditkortsuppgifter online.", "Vilket är ett vanligt sätt för bedragare att stjäla kreditkortsuppgifter online?", 1 },
+                    { 13, "Använd säkra och pålitliga webbplatser, kontrollera att webbadressen börjar med 'https://' och undvik att lagra kreditkortsuppgifter på webbplatser.", "Hur kan du öka säkerheten när du handlar online med ditt kreditkort?", 1 },
+                    { 14, "Regelbunden övervakning av kreditkortsutdrag hjälper till att upptäcka obehöriga transaktioner i tid och minskar risken för bedrägeri.", "Varför är det viktigt att regelbundet övervaka dina kreditkortsutdrag?", 1 },
+                    { 15, "Omedelbart kontakta ditt kreditkortsföretag, spärra ditt kort och rapportera det till polisen för att förebygga obehöriga transaktioner.", "Vilken åtgärd bör vidtas om du misstänker att ditt kreditkort har blivit stulet?", 1 },
+                    { 16, "Undvik att använda betalningsautomater som ser misstänkta ut, täck ditt knappsatsinmatning när du skriver in din PIN-kod och övervaka ditt kreditkortsutdrag.", "Hur kan du undvika att bli offer för skimming på betalningsautomater?", 1 },
+                    { 17, "Oregistrerade eller oseriösa webbplatser, särskilt med onormalt låga priser, kan vara tecken på kreditkortsbedrägeri.", "Vilket är ett vanligt tecken på kreditkortsbedrägeri vid onlineköp?", 1 },
+                    { 18, "CVV-koden används för att bekräfta att du har ditt fysiska kreditkort och bör hållas konfidentiell för att förhindra obehörig användning.", "Varför är det viktigt att inte dela ditt kreditkorts CVV-kod (säkerhetskod) med andra?", 1 },
+                    { 19, "Förvara ditt kreditkort på en säker plats, undvik att lämna det obevakat och använd plånböcker eller skyddsfodral för extra säkerhet.", "Hur kan du säkert förvara ditt fysiska kreditkort för att undvika stöld?", 1 },
+                    { 20, "Omedelbart kontakta ditt kreditkortsföretag, begär att kortet spärras och överväg att ändra ditt lösenord för extra säkerhet.", "Vilken åtgärd bör du vidta om ditt kreditkortsinformation har blivit komprometterad?", 1 }
                 });
 
             migrationBuilder.InsertData(
@@ -225,7 +235,47 @@ namespace ValhallaVault.Migrations
                     { 16, "Använd samma lösenord för alla konton för enkelhetens skull", null, false, 7 },
                     { 17, "Social engineering", null, true, 4 },
                     { 18, "Stark kryptering", null, false, 3 },
-                    { 19, "Brandväggar", null, false, 8 }
+                    { 19, "Brandväggar", null, false, 8 },
+                    { 20, "Kontakta omedelbart kreditkortsföretaget och rapportera transaktionerna.", null, true, 11 },
+                    { 21, "Avvakta och se om det händer igen innan du agerar.", null, false, 11 },
+                    { 22, "Ignorera det och hoppas att det löser sig självt.", null, false, 11 },
+                    { 23, "Publicera informationen på sociala medier för att varna andra.", null, false, 11 },
+                    { 24, "Skimming på betalningsautomater.", null, true, 12 },
+                    { 25, "Användning av säkra webbplatser.", null, false, 12 },
+                    { 26, "Delning av kreditkortsuppgifter via e-post.", null, false, 12 },
+                    { 27, "Användning av offentliga Wi-Fi-nätverk.", null, false, 12 },
+                    { 28, "Använd säkra och pålitliga webbplatser.", null, true, 13 },
+                    { 29, "Lagra kreditkortsuppgifter offentligt.", null, false, 13 },
+                    { 30, "Dela kreditkortsuppgifter via e-post.", null, false, 13 },
+                    { 31, "Använda samma lösenord överallt för bekvämlighet.", null, false, 13 },
+                    { 32, "Förhindra ytterligare skador.", null, true, 14 },
+                    { 33, "Ignorera det och hoppas att det försvinner.", null, false, 14 },
+                    { 34, "Rapportera det till ditt internetleverantörsupport.", null, false, 14 },
+                    { 35, "Kontakta ditt lokala postkontor.", null, false, 14 },
+                    { 36, "Spärra kortet och rapportera till polisen.", null, true, 15 },
+                    { 37, "Avvakta och se om något händer.", null, false, 15 },
+                    { 38, "Publicera informationen på sociala medier.", null, false, 15 },
+                    { 39, "Inget behov av att göra något.", null, false, 15 },
+                    { 40, "Undvik att använda misstänkta betalningsautomater.", null, true, 16 },
+                    { 41, "Använd alltid samma automater.", null, false, 16 },
+                    { 42, "Använd offentliga Wi-Fi-nätverk för att göra inköp.", null, false, 16 },
+                    { 43, "Dela din PIN-kod med andra för säkerhet.", null, false, 16 },
+                    { 44, "Oregistrerade eller oseriösa webbplatser.", null, true, 17 },
+                    { 45, "Enbart välkända och etablerade webbplatser.", null, false, 17 },
+                    { 46, "Webbplatser med höga priser.", null, false, 17 },
+                    { 47, "Webbplatser med stort utbud av produkter.", null, false, 17 },
+                    { 48, "Förhindra obehörig användning.", null, true, 18 },
+                    { 49, "Dela CVV-koden med nära vänner.", null, false, 18 },
+                    { 50, "Publicera CVV-koden online.", null, false, 18 },
+                    { 51, "Använda samma CVV-kod överallt.", null, false, 18 },
+                    { 52, "Förvara det på en säker plats.", null, true, 19 },
+                    { 53, "Lämna det obevakat på offentliga platser.", null, false, 19 },
+                    { 54, "Dela det med okända personer.", null, false, 19 },
+                    { 55, "Använd det som bokmärke.", null, false, 19 },
+                    { 56, "Kontakta kreditkortsföretaget och spärra kortet.", null, true, 20 },
+                    { 57, "Ignorera det och fortsätt använda det som vanligt.", null, false, 20 },
+                    { 58, "Publicera informationen på sociala medier.", null, false, 20 },
+                    { 59, "Använda samma lösenord på andra konton.", null, false, 20 }
                 });
 
             migrationBuilder.CreateIndex(
