b9bd46421fe2ab01ff44c3f1b0f4f1f39c439ec5
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-03-09 18:55:59 +0100

    before switching to a list of AnswerContainer

---
+++ ValhallaVault.Data/Models/AnswerContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValhallaVault.Data.Models;
+
+public class AnswerContainer
+{
+    public string Style { get; set; }
+    public bool IsChecked { get; set; }
+
+    public AnswerModel Answer { get; set; }
+}
+
