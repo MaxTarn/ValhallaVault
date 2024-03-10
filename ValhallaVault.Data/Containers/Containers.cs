63cceb3bfd0e500453c5a8f183f875a8ad693a83
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-03-10 15:23:37 +0100

    added a few classes that are used in the UI, basically a viewModel I think

---
+++ ValhallaVault.Data/Containers/Containers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValhallaVault.Data.Models;
+
+namespace ValhallaVault.Data.Containers;
+
+public class AnswerContainer
+{
+    public bool IsChecked { get; set; }
+    public AnswerModel Original { get; set; }
+    public bool ShowStyle { get; set; } // New property for controlling when to show correct answer
+
+    public string Style => GetAnswerStyle();
+
+    private string GetAnswerStyle()
+    {
+        if (ShowStyle == false) return "";
+
+        if (Original.IsCorrect == true)
+        {
+            return "background-color:green;";
+        }
+        else if (IsChecked)
+        {
+            return "background-color:red;";
+        }
+
+        return "";
+    }
+}
+
+public class QuestionContainer
+{
+    public string Question { get; set; }
+    public List<AnswerContainer> Answers { get; set; }
+    public QuestionModel Original { get; set; }
+}
+
+public class SubCategoryContainer
+{
+    public SubcategoryModel Original { get; set; }
+    public List<QuestionContainer> Questions { get; set; }
+}
+
