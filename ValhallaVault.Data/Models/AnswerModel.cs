1fd138ed7f09260c17317b23c4e3c944c39f6ced
MaxTarn <112612203+MaxTarn@users.noreply.github.com>
2024-03-17 19:36:58 +0100

    added some info string in the Models

--- ValhallaVault.Data/Models/AnswerModel.cs
+++ ValhallaVault.Data/Models/AnswerModel.cs
@@ -14,19 +14,5 @@ public class AnswerModel
     public bool? IsCorrect { get; set; }
 
 
-
-
-
-    /// <summary>
-    /// This will not store any data in the DataBase.
-    /// Only used when getting data and knowing if the user has ticked in this particular answer.
-    /// [[ Yanky as all hell ]]
-    /// but most likely the easiest way to do this(if it actually works the way im thinking?? hopefully)
-    /// Ask Max if this should be deleted before deleting this 
-    /// </summary>
-    public bool? IsChecked { get; set; }
-
-
-
 }
 
--- ValhallaVault.Data/Models/SegmentModel.cs
+++ ValhallaVault.Data/Models/SegmentModel.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Info { get; set; }
         public int CategoryId { get; set; }
         public CategoryModel? Category { get; set; }
         public List<SubcategoryModel>? Subcategories { get; set; } = new();
