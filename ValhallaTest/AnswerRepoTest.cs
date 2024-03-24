59ea392c4c4e87ea08abb2a1ccbad06273c3b455
miiamole <miia_pelaez@hotmail.com>
2024-03-24 12:39:17 +0100

    test för delete som kastar exception

--- ValhallaTest/AnswerRepoTest.cs
+++ ValhallaTest/AnswerRepoTest.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ValhallaVault.Data;
+using ValhallaVault.Data.Models;
 using ValhallaVault.Data.Repositories;
 
 
 namespace ValhallaTest
 {
-
-    //public async Task<IEnumerable<AnswerModel>> GetAllAnswersAsync()
-    //{
-    //    return await _dbContext.Answers.ToListAsync();
-    //}
     public class AnswerRepoTest
     {
         private readonly AnswerRepo _answerRepo;
@@ -52,10 +48,42 @@ namespace ValhallaTest
             // Assert
             Assert.NotNull(actualAnswer);
             Assert.Equal(expectedAnswer.Id, actualAnswer.Id);
+            Assert.Equal(expectedAnswer.Answer, actualAnswer.Answer);
+            //  Assert.NotNull(actualAnswer.Question); troligen onödig pga redan kollat ej null på hela
+            Assert.Equal(expectedAnswer.Question, actualAnswer.Question);
+
         }
 
+        [Fact]
+        public async Task AddAnswerAsync_AddsAnswerToDatabaseTest()
+        {
+            // Arrange
+            var addToThisQuestionId = 2;
+            var addAnswer = new AnswerModel
+            {
+                Answer = "hello",
+                IsCorrect = true,
+                QuestionId = addToThisQuestionId
+            };
 
+            // Act
+            await _answerRepo.AddAnswerAsync(addAnswer);
 
+            // Assert
+            var addedAnswer = await _dbContext.Answers.FindAsync(addAnswer.Id);
+            Assert.NotNull(addedAnswer); // Kolla att svaret har lagts till
+            Assert.Equal(addAnswer.Answer, addedAnswer.Answer);
+            Assert.Equal(addAnswer.IsCorrect, addedAnswer.IsCorrect);
+        }
+
+        [Fact]
+        public async Task DeleteAnswerAsyncTest()
+        {
+            int notAnExcistingId = 2999;
+
+            var exception = await Assert.ThrowsAsync<Exception>(async () => await _answerRepo.DeleteAnswerAsync(notAnExcistingId));
+            Assert.Equal("No answer found with the specified ID.", exception.Message);
+        }
     }
 
 }
