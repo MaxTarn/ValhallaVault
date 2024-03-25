2090f865b8f29957f2b647d5f2cedef50ab23830
miiamole <miia_pelaez@hotmail.com>
2024-03-25 19:58:38 +0100

    försöker mig på ett moq

---
+++ ValhallaTest/ValhallaMoqTest.cs
@@ -0,0 +1,52 @@
+﻿using Autofac.Extras.Moq;
+using ValhallaVault.Data;
+using ValhallaVault.Data.Models;
+
+namespace ValhallaTest
+{
+    public class ValhallaMoqTest
+    {
+        [Fact]
+        public void GetAllAnswersAsyncMoq()
+        {
+            using (var mock = AutoMock.GetLoose()) //med getLoose ser man om en metod har kallats??
+            {
+                mock.Mock<ProgramDbContext>()
+                    .Setup(x => x.Answers)
+                    .Returns(GetAllAnswers());
+            }
+        }
+        private List<AnswerModel> GetAllAnswers()
+        {
+
+
+            List<AnswerModel> AllAnswers = new();
+            {
+                new AnswerModel
+                {
+                    Answer = "hej",
+                    QuestionId = 1,
+                    IsCorrect = true,
+                },
+                new AnswerModel
+                {
+                    Answer = "då",
+                    QuestionId = 2,
+                    IsCorrect = false,
+                },
+                new AnswerModel
+                {
+                    Answer = "mammut",
+                    QuestionId = 1,
+                    IsCorrect = false,
+                },
+                new AnswerModel
+                {
+                    Answer = "dino",
+                    QuestionId = 1,
+                    IsCorrect = true,
+                };
+            }
+        }
+    }
+}
